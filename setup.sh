a3a68938c319fbb439e4e691d9982f61f3611ed7
jizh0u <mail@zhouji.me>
2022-05-22 14:55:38 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -2,6 +2,16 @@
 
 set -ex
 
+# Clean up before setting up
+ip rule del fwmark 666 table 666 || true
+ip route del local 0.0.0.0/0 dev lo table 666 || true
+
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X clash || true
+iptables -t mangle -X clash_local || true
+
 # ENABLE ipv4 forward
 sysctl -w net.ipv4.ip_forward=1
 
